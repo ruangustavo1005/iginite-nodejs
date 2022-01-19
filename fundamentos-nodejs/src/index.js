0dbe0af945e737df98244d8ca9678c59204b578c
ruangustavo1005 <ruan.gustavo.moretti10@gmail.com>
2022-01-18 21:17:36 -0300

    params e insomnia

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
   return response.json({
     message: "Hello, world!!!"
   });
+});
+
+/**
+ * route param -> buscar, alterar e excluir (normalmente id) | OBRIGATÓRIO
+ * query param -> opções/filtros | opcional
+ * body -> objetos com dados para manipulação
+ */
+app.get("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  return response.json({ value : id * 2 });
 });
 
 app.listen(3333);
